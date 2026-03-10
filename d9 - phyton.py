ccaa61f7fb09bbedd7208f5212589cbc0a79241e
Larissa Santana <larissa.santana.docs@gmail.com>
2026-03-10 13:54:44 -0300

    Desafio dia 9

---
+++ d9 - phyton.py
@@ -0,0 +1,9 @@
+nfuncion = int(input())
+nhrstrab = int(input())
+
+valorhr = float(input())
+
+salario = nhrstrab * valorhr
+
+print('NUMBER =',nfuncion)
+print('SALARY = U$ {:.2f}'.format(salario))
